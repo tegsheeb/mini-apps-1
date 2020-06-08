55b9d0f4e0146c39dae2c044601bf34442def6b7
tegshee <tegshbayar@gmail.com>
2020-06-08 11:10:58 -0700

    Add click event handler and put x on each square

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,15 @@
 // window.alert('this is from app');
 console.log('this is from app');
+console.log(document.getElementById('t00'));
+
+function putX(e) {
+  if(e.target.innerHTML === '') {
+    e.target.innerHTML = 'X';
+  } else {
+    document.getElementById('instruction').innerHTML = 'Please press empty place'
+  }
+
+};
+
+const table = document.getElementById('table');
+table.addEventListener('click', putX, false);
