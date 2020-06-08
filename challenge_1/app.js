3b243807b8614ad669707c65bbadee373f6fbaaa
tegshee <tegshbayar@gmail.com>
2020-06-08 11:21:00 -0700

    Alternate btwn X and O

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -8,8 +8,31 @@ function putX(e) {
   } else {
     document.getElementById('instruction').innerHTML = 'Please press empty place'
   }
-
 };
 
+function putO(e) {
+  if(e.target.innerHTML === '') {
+    e.target.innerHTML = 'O';
+  } else {
+    document.getElementById('instruction').innerHTML = 'Please press empty place'
+  }
+};
+
+
+let currentTurn = 'X';
+
+function alternate(e) {
+  if (currentTurn === 'X') {
+    putX(e);
+    currentTurn = 'O';
+    document.getElementById('whoseTurn').innerHTML = 'It is O\'s turn';
+    console.log(currentTurn);
+  } else {
+    putO(e);
+    currentTurn = 'X';
+    document.getElementById('whoseTurn').innerHTML = 'It is X\'s turn';
+  }
+}
+
 const table = document.getElementById('table');
-table.addEventListener('click', putX, false);
+table.addEventListener('click', alternate, false);
