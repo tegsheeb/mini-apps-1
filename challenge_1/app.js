8b6e083001b1d3e1bb1e0d60a953e3e31414836f
tegshee <tegshbayar@gmail.com>
2020-06-08 15:20:23 -0700

    Refactor the code into ES6

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,32 +1,36 @@
-let currentTurn = 'X';
-let totalPlay = 0;
-let gameOver = false;
-let currentBoard = {
-  t00: '', t01: '', t02: '',
-  t10: '', t11: '', t12: '',
-  t20: '', t21: '', t22: ''
+const state = {
+  currentTurn: 'X',
+  totalPlay : 0,
+  gameOver : false,
+  board : {
+    t00: '', t01: '', t02: '',
+    t10: '', t11: '', t12: '',
+    t20: '', t21: '', t22: ''
+  },
+  lastWinner : ''
 }
-let lastWinner = '';
+
+
 
 // add x or o depending on currentTurn
-function playOnce(e, mark) {
+const playOnce = (e, mark) => {
   if(e.target.innerHTML === '') {
-    currentBoard[e.target.id] = mark;
+    state.board[e.target.id] = mark;
     e.target.innerHTML = mark;
-    totalPlay++;
+    state.totalPlay++;
   }
 };
 
-function changeTurn(){
-  if(currentTurn === 'X') {
-    currentTurn = 'O';
+const changeTurn = ()=> {
+  if(state.currentTurn === 'X') {
+    state.currentTurn = 'O';
   } else {
-    currentTurn = 'X';
+    state.currentTurn = 'X';
   }
 }
 
-function checkWinner(turn) {
-  const {t00, t01, t02, t10, t11, t12, t20, t21, t22} = currentBoard
+const checkWinner = (turn) => {
+  const {t00, t01, t02, t10, t11, t12, t20, t21, t22} = state.board
 
   const winCom = [
     [t00, t01, t02],
@@ -40,49 +44,48 @@ function checkWinner(turn) {
 
   for (let i = 0; i < winCom.length; i++) {
     if(winCom[i][0] === turn && winCom[i][1] === turn && winCom[i][2] === turn) {
-      gameOver = true;
-      lastWinner = turn;
+      state.gameOver = true;
+      state.lastWinner = turn;
     }
   }
 };
 
 
-function toggle(e) {
-  if (totalPlay !== 9 && gameOver !== true) {
-    playOnce(e, currentTurn);
-    checkWinner(currentTurn);
+const toggle = (e) => {
+  if (state.totalPlay !== 9 && state.gameOver !== true) {
+    playOnce(e, state.currentTurn);
+    checkWinner(state.currentTurn);
 
-    if(gameOver === true) {
-      var msg = `Game over. ${currentTurn} has won.`
+    if(state.gameOver === true) {
+      var msg = `Game over. ${state.currentTurn} has won.`
       document.getElementById('whoseTurn').innerHTML = msg;
       return;
     }
 
-    if (totalPlay === 9) {
+    if (state.totalPlay === 9) {
       document.getElementById('whoseTurn').innerHTML = 'Board is full and game is a tie. Game over.';
       return;
     }
 
     changeTurn();
-    document.getElementById('whoseTurn').innerHTML = `It is ${currentTurn}\'s turn`;
+    document.getElementById('whoseTurn').innerHTML = `It is ${state.currentTurn}\'s turn`;
 
   }
 }
 
-
-function resetGame() {
-  if (lastWinner === '') {
-    currentTurn = 'X';
-    document.getElementById('whoseTurn').innerHTML = `${currentTurn} starts the game`;
+const resetGame = () => {
+  if (state.lastWinner === '') {
+    state.currentTurn = 'X';
+    document.getElementById('whoseTurn').innerHTML = `${state.currentTurn} starts the game`;
   } else {
-    currentTurn = lastWinner;
-    document.getElementById('whoseTurn').innerHTML = `${lastWinner} won the last time, therefore ${lastWinner} starts the game.`;
+    state.currentTurn = state.lastWinner;
+    document.getElementById('whoseTurn').innerHTML = `The last winner was ${state.lastWinner}, therefore ${state.lastWinner} starts the game.`;
   }
-  totalPlay = 0;
-  gameOver = false;
+  state.totalPlay = 0;
+  state.gameOver = false;
 
-  for (key in currentBoard) {
-    currentBoard[key] = '';
+  for (key in state.board) {
+    state.board[key] = '';
     document.getElementById(`${key}`).innerHTML = '';
   }
 }
@@ -90,7 +93,7 @@ function resetGame() {
 
 // Event listener on board
 const table = document.getElementById('board');
-table.addEventListener('click', toggle, false);
+table.addEventListener('click', toggle , false);
 
 // Reset button
 const reset = document.getElementById('reset');
