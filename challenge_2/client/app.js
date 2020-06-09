4ff7c31e0f3235e5f52962cbf6b60aca08d894bd
tegshee <tegshbayar@gmail.com>
2020-06-09 12:16:39 -0700

    Make sure submit button works

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,15 @@
 
+// function to be invoked on submit
+function sendData(event) {
+  event.preventDefault();
+
+
+}
+
+const form = document.getElementById('form');
+form.addEventListener('submit', sendData);
+
+// post request
+
+
 
