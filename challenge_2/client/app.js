c5af51cc0848e552f24eeacbcb3846ce98be1a39
tegshee <tegshbayar@gmail.com>
2020-06-09 18:06:14 -0700

    Got JSON data from client and parse it

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,8 +1,8 @@
 
 // function to be invoked on submit
 function sendData(event) {
-  event.preventDefault();
-
+  // event.preventDefault();
+  console.log('submit clicked and sending data to server')
 
 }
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -2,9 +2,24 @@ const express = require('express')
 const app = express()
 const port = 3000
 const path = require('path')
+const _ = require('underscore')
+const bodyParser = require('body-parser')
+// const { parse } = require('json2csv');
+
 
 // app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
 app.use('/', express.static(path.join(__dirname, '/client')))
+
+app.use(bodyParser.json())
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.post('/submit', (req, res) => {
+
+  res.send(req.body.form);
+
+})
+
