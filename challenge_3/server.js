3c3a5adbb3d19e5cf9d815d842db6c079b9f110e
tegshee <tegshbayar@gmail.com>
2020-06-10 14:05:33 -0700

    Set up server, babel, serve index.html

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,39 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path')
 
 
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
+app.use('/', express.static(path.join(__dirname, '/public')))
+
+
+
+// app.get('/', (req, res) => res.send('Hello World!'))
+// MAIN TODO
+// 1. Use Express to serve index.html and its assets
+// 2. Use REACT to build UI
+// 3. Use Babel for pre-compile
+// 4. Use MongoDB or MySQL fot database
+
+// Set-up TODO
+// 1. No Webpack, use Babel to --watch
+//   - use Nodemon to watch server.js
+// 2. Express app in server.js
+// 3. Client app in /Client
+// 4. All React component in App.jsx
+// 5. Link transpiled file from index.html.
+
+// Homepage
+// - Homepage with checkout button
+// - F1 & next
+// - F2 & next
+// - F3 & next
+// - Finally, confirmation page which summarizes the data
+// - Purchase button on confirmation page
+// - when purchase completes, user return to homepage
+
+// Database
+// - every time checkout is completes, there is new record in DB.
+// - each step in checkout, saves data into DB (when next is clickec)
