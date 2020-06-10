dd426313937ff6294aa5ffd56b8d8cf7e5e2e393
tegshee <tegshbayar@gmail.com>
2020-06-10 16:27:18 -0700

    Connect database to server

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const path = require('path')
+const db = require('./database')
 
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
